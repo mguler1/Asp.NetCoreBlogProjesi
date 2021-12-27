b3787624682aa6bf7468d534a4fcef815bed69c6
= <=>
2021-12-27 23:04:40 +0300

    category ekleme ve silme işlemleri yapıldı

---
+++ Business/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules
+{
+    public class CategoryValidator: AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Başlık boş geçilemez");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("İçerik boş geçilemez");
+            RuleFor(x => x.CategoryName).MaximumLength(50).WithMessage("En fazla 50 karakter olmalıdır");
+            
+        }
+    }
+}
--- CoreMvcBlog/Areas/Admin/Controllers/CategoryController.cs
+++ CoreMvcBlog/Areas/Admin/Controllers/CategoryController.cs
@@ -1,17 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using X.PagedList;
 namespace CoreMvcBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page=1)
+        {
+            var value = cm.ListAll().ToPagedList(page,5);
+            return View(value);
+        }
+        [HttpGet]
+        public IActionResult CategoryAdd()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category c)
+        {
+            CategoryValidator wv = new CategoryValidator();
+            ValidationResult results = wv.Validate(c);
+            if (results.IsValid)
+            {
+                c.CategoryStatus = true;
+                //c.CategoryDescription=
+                cm.TAdd(c);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+            
+        }
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = cm.GetById(id);
+            cm.TDelete(value);
+            return RedirectToAction("Index");
         }
     }
 }
