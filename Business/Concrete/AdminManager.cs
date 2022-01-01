dfbfaf7c2bde739cdce32ceb51619305f40067da
= <=>
2022-01-01 10:29:33 +0300

    Admin işlemleri katmanlara eklendi

---
+++ Business/Concrete/AdminManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+  public  class AdminManager:IAdminService
+    {
+        IAdminDal _adminDal;
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+
+        public Admin GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
